3a9d984a2f63c334ab6da08fbae33fdc9b5c4923
ASKAR <askarjon.abdullayev@gmail.com>
2024-05-23 19:58:36 +0500

    models

--- apps/links/models.py
+++ apps/links/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    original_url = models.URLField(max_length=600)
+    short_code = models.CharField(max_length=15, blank=True, null=True, unique=True)
+
+    def __str__(self):
+        return self.short_code
